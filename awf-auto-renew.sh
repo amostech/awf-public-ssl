2bc56ccb7b85711613a5370a22a03216eac3c779
Arthur Morales <amostech@gmail.com>
2021-08-19 21:16:22 -0500

    Update awf-auto-renew.sh

--- awf-auto-renew.sh
+++ awf-auto-renew.sh
@@ -6,8 +6,8 @@ MAIL_RECIPIENT=$3
 prerequisites() {
 	wget https://raw.githubusercontent.com/amostech/awf-public-ssl/master/should_renew.py
 	chmod u+x should_renew.py
-	apt-get install -y python-pip
-	pip install pyopenssl
+	apt-get install -y python3-pip
+	pip3 install pyopenssl
 }
 
 prerequisites
